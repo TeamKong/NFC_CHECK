d34f46fc0a313aecf4a87bd5f6e3e7f2a5639f04
sehee.jeong <ppm_it@naver.com>
2018-07-23 16:10:52 +0900

    commit signup ui

--- app/src/main/java/io/kong/incheon/nfc_check/MainActivity.java
+++ app/src/main/java/io/kong/incheon/nfc_check/MainActivity.java
@@ -1,13 +1,44 @@
 package io.kong.incheon.nfc_check;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText input_id, input_pass;
+    Button signin_btn, signup_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        input_id = (EditText) findViewById(R.id.input_id);
+        input_pass = (EditText) findViewById(R.id.input_pass);
+        signin_btn = (Button) findViewById(R.id.signin_btn);
+        signup_btn = (Button) findViewById(R.id.signup_btn);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                switch (view.getId()){
+                    case R.id.signin_btn:
+                        // 로그인
+                        break;
+                    case R.id.signup_btn:
+                        // 회원가입
+                        break;
+                }
+            }
+        };
+
+        signup_btn.setOnClickListener(listener);
+        signin_btn.setOnClickListener(listener);
     }
 }
