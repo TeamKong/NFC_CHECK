cc23b3998d7ba481c8d8aa11a9dd7e8dad8079a1
GeonU <double_k2@naver.com>
2018-08-01 15:33:00 +0900

    modify TimetableActivity
    * Change TextView FontSize
    * TImeTable TextView get ResourceID

--- app/src/main/java/io/kong/incheon/nfc_check/activity/TimeTableActivity.java
+++ app/src/main/java/io/kong/incheon/nfc_check/activity/TimeTableActivity.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import io.kong.incheon.nfc_check.R;
 
-public class TimeTableActivity  extends AppCompatActivity {
+public class TimeTableActivity extends AppCompatActivity {
 
     FloatingActionButton btnFAB;
 
@@ -18,7 +20,19 @@ public class TimeTableActivity  extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timetable);
 
-        btnFAB = (FloatingActionButton)findViewById(R.id.btnFAB);
+        for (int i = 1; i <= 14; i++) {
+            int monResID = getResources().getIdentifier("mon" + i, "id", "io.kong.incheon.nfc_check");
+            int tueResID = getResources().getIdentifier("tue" + i, "id", "io.kong.incheon.nfc_check");
+            int wedResID = getResources().getIdentifier("wed" + i, "id", "io.kong.incheon.nfc_check");
+            int thuResID = getResources().getIdentifier("thu" + i, "id", "io.kong.incheon.nfc_check");
+            int friResID = getResources().getIdentifier("fri" + i, "id", "io.kong.incheon.nfc_check");
+            int satResID = getResources().getIdentifier("sat" + i, "id", "io.kong.incheon.nfc_check");
+
+        }
+
+
+
+        btnFAB = (FloatingActionButton) findViewById(R.id.btnFAB);
         btnFAB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
