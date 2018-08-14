d41cf137e84d9cea43df973f9c7694143589bf72
GeonU <double_k2@naver.com>
2018-08-14 10:06:48 +0900

    Success Join function
    * Completed implementation of membership through mysql and node.js

--- app/src/main/java/io/kong/incheon/nfc_check/activity/SignupActivity.java
+++ app/src/main/java/io/kong/incheon/nfc_check/activity/SignupActivity.java
@@ -1,37 +1,138 @@
 package io.kong.incheon.nfc_check.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import io.kong.incheon.nfc_check.R;
+import io.kong.incheon.nfc_check.item.JoinItem;
+import io.kong.incheon.nfc_check.service.RetrofitService;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class SignupActivity extends AppCompatActivity {
 
-    Spinner spinner;
-    ArrayAdapter sAdapter;
+    static final String TAG_URL = "http://13.209.75.255:3000";
+
+    JoinItem joinItem;
+
+    Spinner sUniversity;
+    Spinner sGrade;
+    Button btnSummit;
+    EditText edID;
+    EditText edPW;
+    EditText edName;
+    EditText edMajor;
+
+    ArrayAdapter saUniversity;
+    ArrayAdapter saGrade;
+
+    Retrofit retrofit;
+
+    String user_id;
+    String user_pw;
+    String user_name;
+    String user_university;
+    String user_major;
+    String user_grade;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        spinner = (Spinner) findViewById(R.id.box_school);
-        sAdapter = ArrayAdapter.createFromResource(this, R.array.question, android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(sAdapter);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        init();
 
+        saUniversity = ArrayAdapter.createFromResource(this, R.array.question, android.R.layout.simple_spinner_dropdown_item);
+        sUniversity.setAdapter(saUniversity);
+
+        saGrade = ArrayAdapter.createFromResource(this,R.array.grade, android.R.layout.simple_spinner_dropdown_item);
+        sGrade.setAdapter(saGrade);
+
+        sUniversity.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?>  parent, View view, int position, long id) {
-//                Toast.makeText(getApplicationContext(), sAdapter.getItem(position), Toast.LENGTH_SHORT).show();
+                user_university = saUniversity.getItem(position).toString();
             }
             public void onNothingSelected(AdapterView<?>  parent) {
             }
         });
 
+        sGrade.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                user_grade = saGrade.getItem(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        btnSummit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insertUser();
+            }
+        });
+    }
+
+    private void init() {
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(TAG_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        joinItem = new JoinItem();
+
+        btnSummit = (Button) findViewById(R.id.btnSummit);
+        edID = (EditText) findViewById(R.id.edID);
+        edPW = (EditText) findViewById(R.id.edPW);
+        edName = (EditText) findViewById(R.id.edName);
+        edMajor = (EditText) findViewById(R.id.edMajor);
+        sUniversity = (Spinner) findViewById(R.id.box_school);
+        sGrade = (Spinner) findViewById(R.id.spiner_Grade);
+
+    }
+
+    private void insertUser() {
+
+        user_id = edID.getText().toString();
+        user_pw = edPW.getText().toString();
+        user_name = edName.getText().toString();
+        user_major = edMajor.getText().toString();
+
+        RetrofitService service = retrofit.create(RetrofitService.class);
+        Call<ResponseBody> call = service.join(user_id, user_pw, user_university, user_name, user_major, user_grade);
+
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if(response.isSuccessful()) {
+                    Toast.makeText(SignupActivity.this, user_id + "님 회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(SignupActivity.this, FirstMenuActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Toast.makeText(SignupActivity.this, "서버 통신에 실패했습니다", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/io/kong/incheon/nfc_check/service/RetrofitService.java
+++ app/src/main/java/io/kong/incheon/nfc_check/service/RetrofitService.java
@@ -6,6 +6,7 @@ import io.kong.incheon.nfc_check.item.UserItem;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
@@ -16,4 +17,7 @@ public interface RetrofitService {
     @GET("user_table")
     Call<List<UserItem>> login(@Query("user_id") String user_id, @Query("user_pw") String user_pw);
 
+    @POST("join")
+    Call<ResponseBody> join(@Query("user_id") String user_id, @Query("user_pw") String user_pw, @Query("user_university") String user_university,
+                            @Query("user_name") String user_name, @Query("user_major") String user_major, @Query("user_grade") String user_grade);
 }
